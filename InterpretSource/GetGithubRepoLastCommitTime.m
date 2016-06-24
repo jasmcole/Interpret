872d308e4f3d491b5c88274eb97409511bf39d4b
Jason Cole <jmc208@dyn1183-174.wlan.ic.ac.uk>
2016-06-24 15:07:54 +0100

    Removed authentication token

--- InterpretSource/GetGithubRepoLastCommitTime.m
+++ InterpretSource/GetGithubRepoLastCommitTime.m
@@ -1,6 +1,6 @@
 function date = GetGithubRepoLastCommitTime(repo)
 
-optionsText = weboptions('username','jasmcole', 'password', 'f56003177f78f893519e7eabc0a77957d3c86bc4');
+optionsText = weboptions('username','jasmcole', 'password', '');
 resp = webread(['https://api.github.com/repos/jasmcole/' repo '/git/refs/heads/master'], optionsText);
 commit = webread(resp.object.url);
 datetime = commit.author.date;
